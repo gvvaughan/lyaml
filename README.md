b7d82cff378ff8e3e75e87bc2d94fb6cf74feaeb
Gary V. Vaughan <gary@gnu.org>
2013-03-17 19:54:02 +0700

    Initial import.
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

---
+++ README.md
@@ -0,0 +1,36 @@
+lyaml
+=====
+
+LibYAML binding for [Lua][].
+
+1. Usage
+--------
+
+    local lyaml   = require "lyaml"
+    local ok      = lyaml.configure (OPTION-NAME, BOOLEAN)
+    local t       = lyaml.load (FILENAME)
+    local yamlstr = lyaml.dump (LUA-TABLE)
+    local null    = lyaml.null ()
+
+The configure function allows enabling and disabling of the following
+options:    
+
+ * `dump_auto_array`
+ * `dump_check_metatables`
+ * `dump_error_on_unsupported`
+ * `load_set_metatables`
+ * `load_numeric_scalars`
+ * `load_nulls_as_nil`
+
+
+2. Instalation
+--------------
+
+    ./configure --prefix=INSTALLATION-ROOT-DIRECTORY
+    make all install
+
+or:
+
+    make
+    make rockspecs
+    luarocks make lyaml-git-1.rockspec
---
+++ lyaml-rockspec.lua
@@ -0,0 +1,44 @@
+-- lyaml rockspec data
+
+-- Variables to be interpolated:
+--
+-- package
+-- version
+
+local default = {
+  package = package_name,
+  version = version.."-1",
+  source = {
+    url = "http://github.com/gvvaughan/"..package_name.."/archive/release-v"..version..".zip",
+    dir = package_name.."-release-v"..version,
+  },
+  description = {
+    summary = "libYAML binding for Lua",
+    detailed = [[
+      Read and write YAML format files with Lua.
+     ]],
+    homepage = "http://github.com/gvvaughan/"..package_name.."/",
+    license = "GPLv3+",
+  },
+  dependencies = {
+    "lua >= 5.1",
+  },
+  external_dependencies = {
+    YAML = { library = "yaml" }
+  },
+  build = {
+    type = "command",
+    build_command = "LUA=$(LUA) LUA_INCLUDE=-I$(LUA_INCDIR) " ..
+      "./configure CPPFLAGS=-I$(YAML_INCDIR) LDFLAGS='-L$(YAML_LIBDIR)' " ..
+      "--prefix=$(PREFIX) --libdir=$(LIBDIR) --datadir=$(LUADIR) " ..
+      "&& make clean && make",
+    install_command = "make install",
+    copy_directories = {},
+  },
+}
+
+if version == "git" then
+  default.build.build_command = "./bootstrap && " .. default.build.build_command
+end
+
+return {default=default, [""]={}}
